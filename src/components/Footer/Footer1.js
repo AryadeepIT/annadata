e94b32a85198419dd8f8690f5ab38da308586e53
Aryadeep Chakraborty <aryadeepit@gmail.com>
2023-04-03 12:53:40 +0530

    bug fixes

--- src/components/Footer/Footer1.js
+++ src/components/Footer/Footer1.js
@@ -4,15 +4,17 @@ import './Footer1.css'
 
 const Footer1 = () => {
   return (
+    
     <div className='footer1'>
         <div className='left'>
             <img src={footer} alt='footer' />
         </div>
         <div className='right'>
-            <h1>Fresh Vegetables, Fruits, Grocery and Many More at Your Doorstep!</h1>
-            <p>Order Groceries from Annadata and Be a part of our family. We Believe in Quality Products.</p>
+            <h1>Fresh Grocery and Many More at Your Doorstep!</h1>
+            <p>Order Groceries from Annadata Now!</p>
         </div>   
     </div>
+    
   )
 }
 
